5857c8bf0ae635defd9212cdad904969df7802b2
Guy <Ofek.r.guy@gmail.com>
2021-01-13 13:08:47 +0200

    computer bug fix

--- Jessy/computer.gd
+++ Jessy/computer.gd
@@ -2,10 +2,10 @@ extends AnimatedSprite
 
 
 func _input(event):
-	if Input.is_action_just_pressed("click") or Input.is_action_just_pressed("click"):
-		if frames.get_frame_count("comp")>(1+frame):
-			frame+=1
-		else:
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("click"):
+		var a =frame
+		frame+=1
+		if frame==a:
 			get_tree().change_scene("res://Scenes/World.tscn")
 		
 
