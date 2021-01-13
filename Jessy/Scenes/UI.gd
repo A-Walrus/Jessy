77661e9d7f68e1f0a5ec9c6e93120e8a8a68956b
Guy <Ofek.r.guy@gmail.com>
2021-01-13 10:19:55 +0200

    Close bed ui

--- Jessy/Scenes/UI.gd
+++ Jessy/Scenes/UI.gd
@@ -1,10 +1,22 @@
 extends Control
+var click=false
+func _process(delta):
+	if Input.is_action_just_pressed("click"):
+		if click:
+			hide()
+			$Center/Bed.hide()
+			set_process(false)
+			click=false
+		click=true
 
-
+func _ready():
+	set_process(false)
 
 func _on_Bed_picked():
+	set_process(true)
 	show()
 	$Center/Bed.show()
+	click=false
 
 
 func _on_sleep_pressed():
