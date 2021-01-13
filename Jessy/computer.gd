394a276720c293ed10bfbe4d6d258710646ef2b1
Guy <Ofek.r.guy@gmail.com>
2021-01-13 10:35:42 +0200

    Computer fix - general

--- Jessy/computer.gd
+++ Jessy/computer.gd
@@ -3,7 +3,8 @@ extends AnimatedSprite
 
 func _input(event):
 	if Input.is_action_just_pressed("click") or Input.is_action_just_pressed("click"):
-		if animation =="home":
-			animation="desktop"
-		elif animation =="desktop":
-			get_tree().change_scene("res://Scenes/World.tscn")
+		frame+=1
+
+
+func _on_computer_animation_finished():
+	get_tree().change_scene("res://Scenes/World.tscn")
