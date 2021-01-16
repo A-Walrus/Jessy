d35d74710bfd9214f34e41bad62e7c47d2839b21
Nir Yanke <nir.yankelovichh@gmail.com>
2021-01-16 19:38:51 +0200

    Makeup

---
+++ Jessy/Scripts/Tools.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+var initpos = position
+var dragable = false
+var grabbed_offset = Vector2()
+var onface = false;
+var steps = ["Blank", "Concealer","Powder","Blush","Lipstick","Eyeliner","Mascara","Blank"]
+var outfit = 1
+var step = 1
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		dragable = event.pressed
+		grabbed_offset = position - get_global_mouse_position()
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and dragable:
+		position = get_global_mouse_position() + grabbed_offset
+	else:
+		position = initpos
+		
+	if Input.is_action_just_released("click") and onface:
+		if(step < 7):
+			get_child(0).animation = steps[step+1] #str(steps[step+1])
+			get_node("../Jessy/Jessy").animation = steps[step]
+			step = step + 1
+
+func _on_Jessy_body_entered(body):
+	onface = true
+
+func _on_Jessy_body_exited(body):
+	onface = false
+	
+#Concealer
+#Powder
+#Blush
+#Lipstick
+#Eyeliner
+#Mascara
