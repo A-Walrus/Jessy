635943b10c9059123a97756db7663e6285854037
Guy <Ofek.r.guy@gmail.com>
2021-01-16 10:41:00 +0200

    minimum distance for clickables

--- Jessy/Scripts/Clickable.gd
+++ Jessy/Scripts/Clickable.gd
@@ -3,20 +3,24 @@ extends Area2D
 export (Array, NodePath) var show = []
 export (String) var scene = ""
 export var mode="C"
-
+export var dist = 300
 
 var inside = false
 
 func _input(event):
 	if not Globals.justClicked:
-		if Input.is_action_just_pressed("click") and inside:
+		if Input.is_action_just_pressed("click") and inside and close():
 			if mode=="C":
 				get_tree().change_scene("res://Scenes/"+scene+".tscn")
 			elif mode=="S":
 				for a in show:
 					get_node(a).show()
 			Globals._on_click()
-	
+
+
+func close():
+	var vec = position - get_node("/root/World/Navigation2D/Jessy").position
+	return vec.length()<dist
 func _on_Clickable_mouse_entered():
 	inside=true
 
