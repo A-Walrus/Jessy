348304ecc44615fa278ddecbf86746769438dc74
Guy <Ofek.r.guy@gmail.com>
2021-01-26 16:09:48 +0200

    drawing bugfix
    drawing in fullscreen used to "screenshot" the drawing bad, now it doesn't

--- Jessy/Scripts/get_drawing.gd
+++ Jessy/Scripts/get_drawing.gd
@@ -7,8 +7,11 @@ extends Sprite
 # Called when the node enters the scene tree for the first time.
 func _exit_tree():
 	if get_tree().get_current_scene().get_name() == "Drawing":
-		region_rect = get_node("/root/Drawing").page_bounds
-		print("helo")
+		var bounds = get_node("/root/Drawing").page_bounds
+		var transform = get_viewport_transform()
+		var sca = transform.get_scale()
+		region_rect = Rect2(transform.xform(bounds.position),transform.xform(bounds.size))
+		scale = Vector2(1/sca.x,1/sca.y)
 		var img = get_viewport().get_texture().get_data()
 		var itex = ImageTexture.new()
 		img.flip_y()
